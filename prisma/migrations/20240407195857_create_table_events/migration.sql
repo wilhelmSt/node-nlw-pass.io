9d66e4249ae7d8bec97a983931666f5a918c7185
Wilhelm Steins <wilhelmsteins@gmail.com>
2024-04-07 17:27:52 -0300

    feat: adding route event

---
+++ prisma/migrations/20240407195857_create_table_events/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "events" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "details" TEXT,
+    "slug" TEXT NOT NULL,
+    "maximum_attendees" INTEGER
+);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,35 @@
-interface Pessoa {
-    nome: string;
-    idade: number;
-}
+import fastify from 'fastify';
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client'
 
-console.log('Hello World!');
+const app = fastify();
+
+const prisma = new PrismaClient({
+  log: ['query'],
+});
+
+app.post('/events', async (request, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(5),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  });
+
+  const data = createEventSchema.parse(request.body);
+
+  const event = await prisma.event.create({
+    data: {
+      title: data.title,
+      details: data.details,
+      maximumAttendees: data.maximumAttendees,
+      slug: new Date().toISOString(),
+    }
+  });
+
+
+  return reply.status(201).send({ eventId: event.id });
+})
+
+app.listen({ port: 3000 }).then(() => {
+  console.log('Server is running on http://localhost:3000');
+})
